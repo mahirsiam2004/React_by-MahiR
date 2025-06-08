c77e31c8e6c5e5dbbb0d369e815b3b0957e4c465
M_FOR_MAHIR <mahirsiam2004@gmail.com>
2025-06-08 21:01:19 +0600

    Initial commit with react projects

---
+++ react-01/src/App.jsx
@@ -0,0 +1,6 @@
+import Greet from "./components/greet";
+
+const App = () => {
+  return <Greet/>;
+};
+export default App;
---
+++ react-01/src/components/Greet.jsx
@@ -0,0 +1,12 @@
+//rafc
+const name = "Mahir";
+const Greet = () => {
+  return (
+    <div>
+      <h1>hello mahir</h1>
+      <p>2+2 = {2 + 2}</p>
+      <p>Noob react code by {name} Siam</p>
+    </div>
+  );
+}; 
+export default Greet;
