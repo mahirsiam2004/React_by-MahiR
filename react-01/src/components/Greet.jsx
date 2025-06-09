1f78c1083d59f48657409648609c1d899fe43eb7
M_FOR_MAHIR <mahirsiam2004@gmail.com>
2025-06-09 10:43:03 +0600

    added and fixed more in react 01

--- react-01/src/components/Greet.jsx
+++ react-01/src/components/Greet.jsx
@@ -1,11 +1,16 @@
 //rafc
 const name = "Mahir";
+const ans=(a,b)=>a*b;
+const specialclass="simple-class";
 const Greet = () => {
   return (
     <div>
       <h1>hello mahir</h1>
       <p>2+2 = {2 + 2}</p>
       <p>Noob react code by {name} Siam</p>
+      <p>5*5 = {ans(5,5)}</p>
+      <p>my friends list {[riyad,shawon,shafayet]}</p>
+      <p className="{specialclass}">this is special class dynamic</p>
     </div>
   );
 }; 
