59ec755a8d907fe034dd3c0932cc7a76b27613c1
M_FOR_MAHIR <mahirsiam2004@gmail.com>
2025-06-10 11:51:12 +0600

    added react 02 usestate

--- react-02/src/App.jsx
+++ react-02/src/App.jsx
@@ -1,14 +1,33 @@
+// import React from 'react';
+
+// import { useState } from "react";
+
+// const App = () => {
+//   const [count, setcount] = useState(0);
+//   const incriment = () => setcount(count + 1);
+//   return (
+//     <section>
+//       <h1>{count}</h1>
+//       <button onClick={incriment}>+</button>
+//     </section>
+//   );
+// };
+
+
+// export default App;
 import React from 'react';
 
 import { useState } from "react";
 
 const App = () => {
-  const [count, setcount] = useState(0);
-  const incriment = () => setcount(count + 1);
+  const [friends, setfriends] = useState(["Riyad","Shafayet"]);
   return (
     <section>
-      <h1>{count}</h1>
-      <button onClick={incriment}>+</button>
+      {
+        friends.map((f)=>(
+          <li key={Math.random()}>{f}</li>
+        ))
+      }
     </section>
   );
 };
